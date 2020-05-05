881197bb75091918e983a64866c7e599b895ca75
srlacerda <diego.lacerda.alves@gmail.com>
2020-05-05 18:12:49 -0300

    commit 2

--- WebAdvert.Web/WebAdvert.Web/Classes/WebsiteProfiles.cs
+++ WebAdvert.Web/WebAdvert.Web/Classes/WebsiteProfiles.cs
@@ -1,10 +1,12 @@
-﻿using AutoMapper;
+﻿using AdvertApi.Models;
+using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAdvert.Web.Models;
 using WebAdvert.Web.Models.AdvertManagement;
+using WebAdvert.Web.Models.Home;
 using WebAdvert.Web.ServiceClients;
 
 namespace WebAdvert.Web.Classes
@@ -15,18 +17,18 @@ namespace WebAdvert.Web.Classes
         {
             CreateMap<CreateAdvertModel, CreateAdvertViewModel>().ReverseMap();
 
-            //CreateMap<AdvertModel, Advertisement>().ReverseMap();
+            CreateMap<AdvertModel, Advertisement>().ReverseMap();
 
-            //CreateMap<Advertisement, IndexViewModel>()
-            //    .ForMember(
-            //        dest => dest.Title, src => src.MapFrom(field => field.Title))
-            //    .ForMember(dest => dest.Image, src => src.MapFrom(field => field.FilePath));
+            CreateMap<Advertisement, IndexViewModel>()
+                .ForMember(
+                    dest => dest.Title, src => src.MapFrom(field => field.Title))
+                .ForMember(dest => dest.Image, src => src.MapFrom(field => field.FilePath));
 
-            //CreateMap<AdvertType, SearchViewModel>()
-            //    .ForMember(
-            //        dest => dest.Id, src => src.MapFrom(field => field.Id))
-            //    .ForMember(
-            //        dest => dest.Title, src => src.MapFrom(field => field.Title));
+            CreateMap<AdvertType, SearchViewModel>()
+                .ForMember(
+                    dest => dest.Id, src => src.MapFrom(field => field.Id))
+                .ForMember(
+                    dest => dest.Title, src => src.MapFrom(field => field.Title));
         }
     }
 }
